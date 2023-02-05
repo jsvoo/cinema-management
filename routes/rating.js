1d911c1c82aaa6b42c085863a43caaa7ae2ea985
Tobi Otoju <ttotoju@gmail.com>
2023-02-05 20:46:54 +0100

    Created routes for ratings,screens,vendors,verifications

---
+++ routes/rating.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+// const homeModel = require('../models/home_page_models')
+const ratings = require('../models/ratings')
+
+
+
+
+
+
+
+
+
+
+
+//card-page
+// router.get("/ratings", (req,res)=>{
+//     res.render("admin/Card-view")
+// })
+
+router.post("/",async (req, res) => {
+   let rating = await  ratings.create(req.body)
+     res.send(rating)
+ })
+ //creating rating
+
+
+
+
+router.get("/ratings", async(req, res) => {
+    let ratings = await ratings.find().lean()
+    res.send({ratings})
+})
+//getting all ratings
+
+
+
+router.get("/ratings/:id", async(req, res) => {
+    let singlerating =  await ratings.findOne({_id:req.params.id})
+    res.send(singlerating)
+})
+//geting single rating id 
+
+
+
+
+
+router.put("/update",async (req, res) => {
+    await  ratings.updateOne({_id:req.params.id}, req.body)
+    res.send(updatedratings)
+
+ })
+ //updating a rating//
+
+
+
+
+ router.delete("/delete",async (req, res) => {
+    //delete method
+    let deleterating =  await ratings.findOne({_id:req.params.id})
+    if(deleterating){
+    await ratings.deleteOne({_id:req.params.id})
+    res.send("ratings deleted")
+    }else{
+    res.send("no ratings")
+    }
+    
+})
+//dleting a rating
+
+
+
+module.exports = router;
+
+
+
+
+
+
+ // // let newData = {
+    // //     Banner_image: "/img/3.jpeg",
+    // //     subtitleText: "The best free stock photos, royalty  free images & videos shared by creators.",
+    // //     action1Text: "Hyper Realism Art",
+    // //     action2Text: "3D PIxels",
+    // //     action2Text: "Wallpapers",
+    // //     Image1: "/img/2.jpeg",
+    // // }//
+
+    // homePageMode.create(newData) //Savee the object to the database using the model class name
+
+    // let homePageRecords = await homePageMode.find()
+
+    // homePageRecords = homePageRecords[0]
+    // console.log(homePageRecords)
